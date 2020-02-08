74fce16200a28226e7335d78e8204f1d8866a46b
James Nyika <jnyika@gmail.com>
2020-02-08 10:26:56 -0500

    Added servers...

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/java.jdbc "0.7.11"]
+                 [http-kit "2.3.0"]
                  [java-jdbc/dsl "0.1.3"]
-                 [org.apache.derby/derby "10.14.2.0"]])
+                 [clj-time "0.14.0"]
+                 [compojure "1.6.0"]
+                 [org.apache.derby/derby "10.14.2.0"]]
 
-                 
+  :main svr.core)
--- src/svr/core.clj
+++ src/svr/core.clj
@@ -1,5 +1,9 @@
 (ns svr.core
   (:require [clojure.java.jdbc :as jdbc]
+            [org.httpkit.server :refer [run-server]]
+            [clj-time.core :as t]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
             [java-jdbc.ddl :as ddl]
             [java-jdbc.sql :as sql]))
 
@@ -12,3 +16,50 @@
               ;; :user "bilbo"
               ;; :password "secret"
               })
+
+(defn get-time
+  []
+  (let [response {:status  200
+                  :headers {"Content-Type" "text/html"}
+                  :body    (str (t/time-now))}]
+    response))
+
+; returns current date.
+(defn get-date
+  [greet-name]
+  (let [response {:status  200
+                  :headers {"Content-Type" "text/html"}
+                  :body    (str (.getTime (java.util.Date.)))}]
+    response))
+
+; define routes.
+(defroutes app
+           (GET "/" [] "<h1>Good morning Hashicorp!!!! Let's do this...</h1>")
+           (GET "/get-time" [] (get-time))
+           (GET "/get-date" [] (get-date))
+           (route/not-found "<h1>Whow! Either you do not have this route or Consul says NO! ..</h1>"))
+
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "HASHI-SVR1 is up  - sponsored by HTTPKIT"})
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    ;; graceful shutdown: wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(defn -main [& args]
+  ;; The #' is useful when you want to hot-reload code
+  ;; You may want to take a look: https://github.com/clojure/tools.namespace
+  ;; and http://http-kit.org/migration.html#reload
+  (println ":=====> Starting HTTPKit Server on port 8080")
+  (reset! server
+    (run-server #'app {:port 8080}))
+  (println ":=====> HASHISERVER started: Port 8080 <========")
+    )
