3aa78cf053d5263c3d8da1092ee77b45b237ab78
James Nyika <jnyika@gmail.com>
2020-02-10 13:22:06 -0500

    file permission change

--- scripts/tstappsvr-proxy.sh
+++ scripts/tstappsvr-proxy.sh
@@ -4,4 +4,7 @@
 ## -service can have any name you like
 ## -upstream  service-name : desired listen port  (do not provide ID but rather service name )
 
+echo "Starting a testing service to try out connection to the appsvr proxy on port 9191"
+echo "You can use this link to test it out : curl http://127.0.0.1:9191/list-posts "
+echo "  -----   "
 consul connect proxy -service appsvrdev -upstream appsvr:9191
