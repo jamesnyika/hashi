9200c0cac84a109de7e7e7bdb9cfdd23de6c3c5f
James Nyika <jnyika@gmail.com>
2020-02-10 14:39:45 -0500

    all done. Now the intentions

--- scripts/dbsvr-proxy.sh
+++ scripts/dbsvr-proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Starting sidecar proxy for derby (DB Server) service..."
+echo "Starting sidecar proxy for derby (DB Server) service only..."
 consul connect proxy -sidecar-for derby
--- scripts/startproxies.sh
+++ scripts/startproxies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting sidecar proxy for httpkit (appsvr) service..."
+# use IDs not names of services. Bug requires lowercase names
 consul connect proxy -sidecar-for httpkit &
 
 echo "Starting sidecar proxy for derby (dbserver) service..."
+# use IDs not names of services. Bug requires lowercase names 
 consul connect proxy -sidecar-for derby &
