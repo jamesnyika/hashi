f6c03daed89df402d4088a933cd64d9c182ca2bc
James Nyika <jnyika@gmail.com>
2020-02-12 19:27:17 -0500

    Working configuration

--- scripts/startdb.sh
+++ scripts/startdb.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 echo "Starting Derby Network DB server on all interfaces and port:1527.."
 
-source ~/.bash_profile
+if [ -z "$JAVA_HOME" ]
+then
+      export JAVA_HOME=$(/usr/libexec/java_home)
+else
+      echo "..JAVA_HOME is set.. continuing"
+fi
 
-../derby/bin/startNetworkServer -h 0.0.0.0 -p 1527
+## may have it already
+## source ~/.bash_profile
+
+../derby/bin/startNetworkServer -h 0.0.0.0 -p 1527 &
